104a3fbb4784c68921603e083425617c242f0e4b
Carlos Herrera <carlos4505@gmail.com>
2023-05-19 17:34:00 -0500

    Objetos en Python y PHP

--- PHP/car.php
+++ PHP/car.php
@@ -1,7 +1,17 @@
 <?php
+
 class Car {
-public $id = integer;
-public $license = string;
-public $driver = string;
-public $passengers = integer;
+  public $id;
+  public $license;
+  public $driver;
+  public $passengers;
+
+  public function __construct($license, $driver) {
+    $this->license = $license;
+    $this->driver = $driver;
+  }
+
+  public function PrintDataCar(){
+    echo "license: $this->license, conductor: {$this->driver->name}, document: {$this->driver->document}";
+  }
 }
