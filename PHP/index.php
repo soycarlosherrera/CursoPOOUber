232bdc91339c9301a86755cf79ea12d70ddf3369
Carlos Herrera <carlos4505@gmail.com>
2023-05-19 18:12:12 -0500

    Clases heredadas en Java y PHP

--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,12 @@
 <?php
+require_once("car.php");
+require_once("uberX.php");
+require_once("uberPool.php");
+require_once("account.php");
 
-require_once('Car.php');
-require_once('Account.php');
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX->printDataCar();
 
-$car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
-$car->printDataCar();
+$uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dodge", "Attitude");
+$uberPool->printDataCar();
+?>
