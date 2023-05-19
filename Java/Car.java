3f6d775fca04dad833cbba7e001983bd799965d0
Carlos Herrera <carlos4505@gmail.com>
2023-05-19 14:26:25 -0500

    Agregando objetos en Java y Python

--- Java/Car.java
+++ Java/Car.java
@@ -3,4 +3,9 @@ class Car {
     String license;
     String driver;
     Integer passegenger;
+
+    void printDataCar(){
+        System.out.println("Licence: "+license+" Driver: "+driver);
+    }
+
 }
